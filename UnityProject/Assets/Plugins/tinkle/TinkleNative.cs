6a56a9ed63b4e75cd84da124a9128123d6374f37
c6burns <chris@fursman.com>
2019-03-13 21:13:53 -0600

    added basic unity project and test for GC.Alloc marker

---
+++ UnityProject/Assets/Plugins/tinkle/TinkleNative.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class TinkleNative : MonoBehaviour
+{
+    private byte[] _snaco;
+
+    [DllImport("tnkl", CharSet = CharSet.Ansi)]public static extern void tinkletytinkerz(StringBuilder str, ulong sz);
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        _snaco = new byte[1024];
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Retreieve Markers"))
+        {
+            StringBuilder bigStr = new StringBuilder(9857893, 9857893);
+            TinkleNative.tinkletytinkerz(bigStr, 9857893);
+            Debug.Log(bigStr.ToString());
+        }
+        
+    }
+}
--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,24 +1,38 @@
 #include "IUnityInterface.h"
 #include "IUnityProfilerCallbacks.h"
 
-
+#include <algorithm>
 #include <string>
 
 static IUnityProfilerCallbacks *s_UnityProfilerCallbacks = NULL;
 static UnityProfilerMarkerDesc gcAllocDesc;
+static std::string giantString;
+
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API tinkletytinkerz(char *out_str, size_t out_len)
+{
+	const size_t cpysz = std::min<size_t>(giantString.length(), out_len);
+	giantString.copy(out_str, cpysz, 0);
+	return;
+}
 
 void UNITY_INTERFACE_API createCategoryCallback(const UnityProfilerCategoryDesc* categoryDesc, void* userData)
 {
 	return;
 }
 
-void UNITY_INTERFACE_API createMarkerCallback(const UnityProfilerMarkerDesc* markerDesc, void* userData)
+void UNITY_INTERFACE_API markerEventCallback(const UnityProfilerMarkerDesc* markerDesc, UnityProfilerMarkerEventType eventType, uint16_t eventDataCount, const UnityProfilerMarkerData* eventData, void* userData)
 {
 	return;
 }
 
-void UNITY_INTERFACE_API markerEventCallback(const UnityProfilerMarkerDesc* markerDesc, UnityProfilerMarkerEventType eventType, uint16_t eventDataCount, const UnityProfilerMarkerData* eventData, void* userData)
+void UNITY_INTERFACE_API createMarkerCallback(const UnityProfilerMarkerDesc* markerDesc, void* userData)
 {
+	giantString.append(markerDesc->name);
+	giantString.push_back('\n');
+	if (markerDesc->name == "GC.Alloc") {
+		memcpy(&gcAllocDesc, markerDesc, sizeof(gcAllocDesc));
+		s_UnityProfilerCallbacks->RegisterMarkerEventCallback(markerDesc, markerEventCallback, NULL);
+	}
 	return;
 }
 
@@ -32,18 +46,22 @@ void UNITY_INTERFACE_API threadCallback(const UnityProfilerThreadDesc* threadDes
 	return;
 }
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces *unityInterfaces)
 {
 	s_UnityProfilerCallbacks = unityInterfaces->Get<IUnityProfilerCallbacks>();
 
 	memset(&gcAllocDesc, 0, sizeof(gcAllocDesc));
 	gcAllocDesc.name = "GC.Alloc";
-	s_UnityProfilerCallbacks->RegisterMarkerEventCallback(&gcAllocDesc, markerEventCallback, NULL);
 	s_UnityProfilerCallbacks->RegisterCreateMarkerCallback(createMarkerCallback, NULL);
+	s_UnityProfilerCallbacks->RegisterFrameCallback(frameCallback, NULL);
+	s_UnityProfilerCallbacks->RegisterCreateThreadCallback(threadCallback, NULL);
+	s_UnityProfilerCallbacks->RegisterCreateCategoryCallback(createCategoryCallback, NULL);
 }
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
 {
-	s_UnityProfilerCallbacks->UnregisterMarkerEventCallback(&gcAllocDesc, markerEventCallback, NULL);
+	s_UnityProfilerCallbacks->UnregisterFrameCallback(frameCallback, NULL);
 	s_UnityProfilerCallbacks->UnregisterCreateMarkerCallback(createMarkerCallback, NULL);
+	s_UnityProfilerCallbacks->UnregisterCreateThreadCallback(threadCallback, NULL);
+	s_UnityProfilerCallbacks->UnregisterCreateCategoryCallback(createCategoryCallback, NULL);
 }
